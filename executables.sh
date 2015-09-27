92693ee9d11083be4ed62997a91d06f206ee56aa
Victor Engmark <victor.engmark@gmail.com>
2015-09-27 13:38:21 +0100

    List files in common executables directories

---
+++ executables.sh
@@ -0,0 +1,19 @@
+set -o errexit -o nounset
+
+for dir in {,/usr{,/local}}/{,s}bin
+do
+    if [ -L "$dir" ]
+    then
+        echo "${dir} -> $(readlink --canonicalize "$dir")"
+    elif [ -d "$dir" ]
+    then
+        echo "${dir}:"
+        ls "$dir"
+    elif [ -e "$dir" ]
+    then
+        echo "${dir} is neither a symlink not a directory:"
+        ls -l "$dir"
+    else
+        echo "${dir} does not exist"
+    fi
+done
