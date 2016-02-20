ffc76b284327bd5d89399cd04870ac5eef249c89
Victor Engmark <victor.engmark@gmail.com>
2016-02-20 00:47:30 +0000

    Show the detailed state of the SSH directory after finishing

--- ssh.sh
+++ ssh.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o noclobber
 
-ls ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 127.0.0.1 echo Success
+ls -l ~/.ssh
