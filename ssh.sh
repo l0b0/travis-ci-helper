1812b485818280c2ae1f34ad1f9beb45371f4273
Victor Engmark <victor.engmark@gmail.com>
2016-02-20 00:26:48 +0000

    Don't mention new key explicitly

--- ssh.sh
+++ ssh.sh
@@ -5,4 +5,4 @@ set -o errexit
 ls ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-ssh -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost echo Success
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost echo Success
