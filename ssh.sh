ba08f9598be4208b85de94474ee02f97eef65e5f
Victor Engmark <victor.engmark@gmail.com>
2016-02-20 00:45:12 +0000

    Use local IP address

--- ssh.sh
+++ ssh.sh
@@ -5,4 +5,4 @@ set -o errexit
 ls ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost echo Success
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no 127.0.0.1 echo Success
