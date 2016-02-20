6d9d498bb29447600ae2b95e17929f86f422277f
Victor Engmark <victor.engmark@gmail.com>
2016-02-20 00:23:22 +0000

    Authorize new key

--- ssh.sh
+++ ssh.sh
@@ -4,4 +4,5 @@ set -o errexit
 
 ls ~/.ssh
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -P ''
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 ssh -i ~/.ssh/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no localhost echo Success
